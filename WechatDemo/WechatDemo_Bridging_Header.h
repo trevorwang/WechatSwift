1cfb5a8e11d55a331bd8dae657971cf9e2272266
Trevor Wang <oumei.vip@gmail.com>
2015-01-04 18:17:33 +0800

    init

---
+++ WechatDemo/WechatDemo_Bridging_Header.h
@@ -0,0 +1,16 @@
+//
+//  WechatDemo_Bridging_Header_h.h
+//  WechatDemo
+//
+//  Created by Ming Wang on 1/4/15.
+//  Copyright (c) 2015 Ming Wang. All rights reserved.
+//
+
+#ifndef WechatDemo_WechatDemo_Bridging_Header_h_h
+#define WechatDemo_WechatDemo_Bridging_Header_h_h
+
+@import UIKit;
+#import "WXApiObject.h"
+#import "WXApi.h"
+
+#endif
